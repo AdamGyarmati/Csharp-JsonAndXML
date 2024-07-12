0836bf9e9f5829f5be0ce7c50c95c132f808a75c
Adam Gyarmati <gyari12@gmail.com>
2024-07-12 20:36:58 +0200

    add some example

---
+++ JsonAndXML/JsonClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAndXML
+{
+    public class JsonClass
+    {
+        public int Egesz { get; set; }
+        public double Tort { get; set; }
+
+        public override string ToString()
+        {
+            return $"Egesz: {Egesz}, Tort: {Tort}";
+        }
+    }
+}
--- JsonAndXML/Program.cs
+++ JsonAndXML/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JsonAndXML;
+using Newtonsoft.Json;
+using System.Text;
+using System.Xml.Serialization;
+
+//XmlSerializer serializer = new XmlSerializer(typeof(XMLClass));
+
+//using (MemoryStream teszt = new MemoryStream())
+//{
+//    XMLClass kiirando = new XMLClass
+//    {
+//        Egesz = 42,
+//        Tort = 3.1415
+//    };
+//    Console.WriteLine("Írás előtt:");
+//    Console.WriteLine(kiirando);
+
+//    serializer.Serialize(teszt, kiirando);
+
+//    //visszaállunk a stream elejére
+//    //az írás után a végén vagyunk
+//    teszt.Seek(0, SeekOrigin.Begin);
+
+//    Console.WriteLine("XML adat:\n");
+//    using (var reader = new StreamReader(teszt, Encoding.UTF8, false, 1024, true))
+//    {
+//        Console.WriteLine(reader.ReadToEnd());
+//    }
+
+//    //visszaállunk ismét a stream elejére
+//    teszt.Seek(0, SeekOrigin.Begin);
+
+//    Console.WriteLine("\nOlvasás után: ");
+//    XMLClass beolvasott = (XMLClass)serializer.Deserialize(teszt);
+//    Console.WriteLine(beolvasott);
+//}
+
+
+JsonClass kiirando = new JsonClass
+{
+    Egesz = 42,
+    Tort = 3.1415
+};
+string json = JsonConvert.SerializeObject(kiirando, Formatting.Indented);
+
+Console.WriteLine("Json:");
+Console.WriteLine(json);
+
+JsonClass vissza = JsonConvert.DeserializeObject<JsonClass>(json);
+
+Console.WriteLine(vissza);
